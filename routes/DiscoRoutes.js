82d40a32dffe75f39ced6088ae3cacb5834d7a6a
tskbelli <isabellirosa.bg002@academico.ifsul.edu.br>
2025-12-04 09:12:55 -0300

    commit número indefinido

--- routes/DiscoRoutes.js
+++ routes/DiscoRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 const router = express.Router();
-//Busca o AlunoController
 import DiscoController from '../controllers/DiscoController.js'
 const controle = new DiscoController();
 import multer from 'multer';
@@ -8,14 +7,19 @@ import multer from 'multer';
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+// Garanta que este caminho base bata com o do Controller (adm/disco/)
 const caminhobase = 'adm/disco/'
 
 router.get('/' + caminhobase + 'add', controle.openAdd)
 router.post('/' + caminhobase + 'add', upload.single('img'), controle.add)
+
 router.get('/' + caminhobase + 'lst', controle.list)
 router.post('/' + caminhobase + 'lst', controle.find)
+
 router.get('/' + caminhobase + 'del/:id', controle.del)
-router.get('/' + caminhobase + 'edt/:id', upload.single('img'), controle.openEdt)
-router.post('/' + caminhobase + 'edt/:id', controle.edt)
+
+router.get('/' + caminhobase + 'edt/:id', controle.openEdt)
+// AQUI ESTAVA O ERRO: Faltava o upload.single('img') nesta linha abaixo
+router.post('/' + caminhobase + 'edt/:id', upload.single('img'), controle.edt)
 
 export default router
